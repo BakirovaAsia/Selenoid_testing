3cfdd5d95ee87c678a951325eba353bcac5f9c73
BakirovaAsia <bakirova.asia@gmail.com>
2021-06-08 14:50:48 +0300

    add 89

--- tests/capabilities.py
+++ tests/capabilities.py
@@ -1,8 +1,18 @@
-capabilities = {
+capabilities ={
+    {
     "browserName": "chrome",
     "browserVersion": "90.0",
     "selenoid:options": {
         "enableVNC": True,
         "enableVideo": False
         }
+    },
+    {
+    "browserName": "chrome",
+    "browserVersion": "89.0",
+    "selenoid:options": {
+        "enableVNC": True,
+        "enableVideo": False
+        }
     }
+}

3221d8bde907e6140b375be88654cd7c90111ae2
bakirovaasia <bakirova.asia@gmail.com>
2021-07-26 10:03:27 +0300

    log

--- tests/login_as_adm_test.py
+++ tests/login_as_adm_test.py
@@ -28,17 +28,17 @@ def test_login_admin():
     #driver = webdriver.Chrome(executable_path="./chromedriver", options=chrome_options)
     driver.get(base_url)
     logger.info('Waiting url: %s' % ('%smain' % base_url))
-    assert True
-    #WebDriverWait(driver, 10).until(EC.url_contains('%smain' % base_url))
     
-    #logger.info('Enter login')
-    #WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'login'))).send_keys('1')
-    #logger.info('Enter login')
-    #WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'password'))).send_keys('1')
-    #logger.info('Press button')
-    #WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button.MuiButtonBase-root.MuiButton-root.MuiButton-contained.jss16.MuiButton-containedPrimary.MuiButton-fullWidth'))).click()
-    #logger.info('Waiting login')
-    #assert WebDriverWait(driver, 10).until(EC.url_contains('%smain' % base_url)), 'main'
+    WebDriverWait(driver, 10).until(EC.url_contains('%smain' % base_url))
+    
+    logger.info('Enter login')
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'login'))).send_keys('1')
+    logger.info('Enter login')
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'password'))).send_keys('1')
+    logger.info('Press button')
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button.MuiButtonBase-root.MuiButton-root.MuiButton-contained.jss16.MuiButton-containedPrimary.MuiButton-fullWidth'))).click()
+    logger.info('Waiting login')
+    assert WebDriverWait(driver, 10).until(EC.url_contains('%smain' % base_url)), 'main'
    
 
 if __name__ == '__main__':
